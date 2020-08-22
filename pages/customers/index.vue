c3d4cc4f51f2f63a00d39196f00d3a3c9a813737
Gerson E. Aguirre <hola@gersonaguirre.me>
2020-08-22 12:11:33 -0600

    CUSTOMER PREVIEW
    Showing information for customer preview

--- pages/customers/index.vue
+++ pages/customers/index.vue
@@ -7,6 +7,87 @@
       { name: 'Listado de clientes', to: null },
     ]"
   >
+    <el-dialog
+      title="Vista previa"
+      :visible.sync="showCustomerPreview"
+      width="900px"
+      :append-to-body="true"
+    >
+      <div class="flex flex-col space-y-2">
+        <div class="grid grid-cols-12 gap-4">
+          <div class="col-span-6 flex flex-col">
+            <span class="font-semibold">Nombre o razón social</span>
+            <span>{{ selectedCustomer ? selectedCustomer.name : ''}}</span>
+          </div>
+          <div class="col-span-2 flex flex-col">
+            <span class="font-semibold">Identificador</span>
+            <span>{{ selectedCustomer ? selectedCustomer.shortName : ''}}</span>
+          </div>
+          <div class="col-span-2 flex flex-col">
+            <span class="font-semibold">Estado</span>
+            <div v-if="selectedCustomer">
+              <el-tag size="small" type="success" v-if="selectedCustomer.isActiveCustomer">Activo</el-tag>
+              <el-tag size="small" type="warning" v-else>Inactivo</el-tag>
+            </div>
+          </div>
+          <div class="col-span-2 flex flex-col">
+            <span class="font-semibold">Es proveedor</span>
+            <div>
+              <el-tag
+                size="small"
+                type="warning"
+                class="w-auto"
+              >{{ selectedCustomer ? selectedCustomer.isProvider ? 'SI' : 'NO' : ''}}</el-tag>
+            </div>
+          </div>
+        </div>
+        <div class="grid grid-cols-12 gap-4">
+          <div
+            class="col-span-2 flex flex-col"
+            v-if="selectedCustomer && selectedCustomer.customerTaxerType == 2"
+          >
+            <span class="font-semibold">DUI</span>
+            <span>{{ selectedCustomer ? selectedCustomer.dui : ''}}</span>
+          </div>
+          <div class="col-span-3 flex flex-col">
+            <span class="font-semibold">NIT</span>
+            <span>{{ selectedCustomer ? selectedCustomer.nit : ''}}</span>
+          </div>
+          <div class="col-span-2 flex flex-col">
+            <span class="font-semibold">NRC</span>
+
+            <span>{{ selectedCustomer ? selectedCustomer.nrc : ''}}</span>
+          </div>
+          <div class="col-span-5 flex flex-col">
+            <span class="font-semibold">GIRO</span>
+
+            <span>{{ selectedCustomer ? selectedCustomer.giro : ''}}</span>
+          </div>
+        </div>
+        <div class="grid grid-cols-12 gap-4">
+          <div class="col-span-3 flex flex-col">
+            <span class="font-semibold">Tipo de cliente</span>
+            <span>{{ selectedCustomer ? selectedCustomer.customerType.name : ''}}</span>
+          </div>
+          <div class="col-span-3 flex flex-col">
+            <span class="font-semibold">Tipo de contribuyente</span>
+            <span>{{ selectedCustomer ? selectedCustomer.customerTaxerType.name : ''}}</span>
+          </div>
+          <div
+            class="col-span-3 flex flex-col"
+            v-if="selectedCustomer && selectedCustomer.customerTaxerType == 2"
+          >
+            <span class="font-semibold">Tipo de persona natural</span>
+
+            <span>{{ selectedCustomer.customerTypeNatural.name }}</span>
+          </div>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="showCustomerPreview = false" size="small">Cerrar</el-button>
+      </span>
+    </el-dialog>
+
     <div class="flex flex-col space-y-2">
       <div class="flex justify-center" v-if="errorMessage">
         <Notification
@@ -56,21 +137,12 @@
       <el-table :data="customers.customers" stripe size="mini">
         <el-table-column type="index" min-width="40" />
         <el-table-column label="Nombre" prop="name" min-width="350" />
-        <el-table-column
-          label="Tipo"
-          prop="customerType.name"
-          min-width="120"
-        />
+        <el-table-column label="Tipo" prop="customerType.name" min-width="120" />
         <el-table-column label="NIT" prop="nit" min-width="160" />
         <el-table-column label="NRC" prop="nrc" min-width="90" />
         <el-table-column label="Estado" min-width="90">
           <template slot-scope="scope">
-            <el-tag
-              size="small"
-              type="success"
-              v-if="scope.row.isActiveCustomer"
-              >Activo</el-tag
-            >
+            <el-tag size="small" type="success" v-if="scope.row.isActiveCustomer">Activo</el-tag>
             <el-tag size="small" type="warning" v-else>Inactivo</el-tag>
           </template>
         </el-table-column>
@@ -79,9 +151,7 @@
             <el-dropdown trigger="click">
               <el-button icon="el-icon-more" size="small" />
               <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item
-                  @click.native="showCustomerPreview(scope.row)"
-                >
+                <el-dropdown-item @click.native="openCustomerPreview(scope.row)">
                   <i class="el-icon-view"></i> Vista previa
                 </el-dropdown-item>
                 <el-dropdown-item
@@ -95,7 +165,9 @@
                   <span v-if="scope.row.isActiveCustomer">
                     <i class="el-icon-close"></i> Desactivar
                   </span>
-                  <span v-else> <i class="el-icon-check"></i> Activar </span>
+                  <span v-else>
+                    <i class="el-icon-check"></i> Activar
+                  </span>
                   cliente
                 </el-dropdown-item>
                 <!-- <el-dropdown-item>
@@ -169,6 +241,8 @@ export default {
         limit: 10,
         page: 1,
       },
+      showCustomerPreview: false,
+      selectedCustomer: null,
     };
   },
   methods: {
@@ -194,8 +268,8 @@ export default {
       this.page.limit = val;
       this.fetchCustomers();
     },
-    changeActive({ id, active }) {
-      const action = active ? "desactivar" : "activar";
+    changeActive({ id, isActiveCustomer }) {
+      const action = isActiveCustomer ? "desactivar" : "activar";
       this.$confirm(
         `¿Estás seguro que deseas ${action} este cliente?`,
         "Confirmación",
@@ -208,7 +282,7 @@ export default {
               instance.confirmButtonLoading = true;
               instance.confirmButtonText = "Procesando...";
               this.$axios
-                .put(`/customers/status/${id}`, { status: !active })
+                .put(`/customers/status/${id}`, { status: !isActiveCustomer })
                 .then((res) => {
                   this.$notify.success({
                     title: "Éxito",
@@ -270,6 +344,11 @@ export default {
           },
         }
       );
+    },
+    async openCustomerPreview({ id }) {
+      const { data } = await this.$axios.get(`/customers/${id}`);
+      this.selectedCustomer = data.customer;
+      this.showCustomerPreview = true;
     },
   },
 };
