cf1f24e32343afe88370aa0b9a0680fede26656d
Jorge <jobeta1996@gmail.com>
2020-12-21 18:02:31 -0600

    OC-45: - cambien el dise;o de los formulario.

--- pages/invoices/index.vue
+++ pages/invoices/index.vue
@@ -107,7 +107,7 @@
     </el-option>
   </el-select>
         </div>
-        <div class=" col-span-3 w-56">
+        <div class=" col-span-3 w-56"
           <span class="text-gray-700 text-xs" > Zona:</span>
           <el-select v-model="value" 
            size="small"
