03f8ce530e2e419c2018538de0a809c54737a197
Gerson E. Aguirre <hola@gersonaguirre.me>
2020-12-26 11:06:46 -0600

    DATE-FNS ADDED

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,11 @@ export default {
     "./plugins/filters.js",
     "./plugins/vue-debounce.js",
   ],
-  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  buildModules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/date-fns",
+  ],
   modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
   router: {
     middleware: ["auth"],
