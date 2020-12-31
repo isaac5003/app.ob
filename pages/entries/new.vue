3c9eff25fc0ff6132be8bafdae05a93570db056a
Isaac <isaac.cortez1294@hotmail.com>
2020-12-30 21:27:16 -0600

    OC-64: -Empezando a trabajar la vista

--- pages/entries/new.vue
+++ pages/entries/new.vue
@@ -7,7 +7,50 @@
       { name: 'Nueva partida', to: null }
     ]"
   >
-  <h1>Hola mundo</h1>
+   <div class="flex flex-col space-y-2">
+     <el-form label-position="top">
+          <div class="grid grid-cols-12 gap-4  relative">
+            <div class=" col-start-1 col-span-4 ">
+              <el-form-item label="* Tipo de patidad">
+                 <el-select 
+                 v-model="value" 
+                 size="small"
+                 class="w-full"
+                 placeholder="Select">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              </el-form-item>
+            </div>
+            <div class="col-start-9 col-span-2">
+              <el-form-item label="N° de correlativo">
+                <el-input
+                  placeholder="Please input"
+                  v-model="input"
+                  :disabled="true"
+                  class="w-full"
+                  size="small">
+                </el-input>
+              </el-form-item>
+            </div>
+           <div class="col-start-11 col-span-3 absolute">
+            <el-form-item label="Rango de fechas:">
+              <el-date-picker
+                v-model="option.name"
+                size="small"
+                style="width:100%"
+                type="date"
+              >
+              </el-date-picker>
+            </el-form-item>
+          </div>
+          </div>
+     </el-form>
+   </div>
   </layout-content>
 </template>
 
@@ -31,6 +74,9 @@ export default {
   data() {
     return {
       loading: false,
+      option:{
+        name:""
+      }
     };
   },
   methods: {
