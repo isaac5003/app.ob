69dc649cf6bb8f4aef800bba18e57d190d526b32
Jorge <jobeta1996@gmail.com>
2021-01-06 20:34:59 -0600

    OC-72: -Vista previa.

--- pages/invoices/index.vue
+++ pages/invoices/index.vue
@@ -7,7 +7,7 @@
       { name: 'Listado de ventas', to: null },
     ]"
   >
-    <div class="flex flex-col">
+    <div class="flex flex-col space-4">
       <div class="flex justify-center" v-if="errorMessage">
         <Notification
           class="w-1/2"
@@ -21,20 +21,15 @@
         />
       </div>
       <el-form label-position="top">
-        <div class="grid grid-cols-12">
+        <div class="grid grid-cols-12 gap-4">
           <div class=" col-start-10 col-span-4">
             <el-form-item>
-                 <el-input
-              suffix-icon="el-icon-search"
-              placeholder="Buscar..."
-              v-model="searchValue.searchValue"
-              size="small"
-              style="margin-top: 26px"
-              clearable
-              class="w-full"
-              v-debounce:500ms="fetchInvoices"
-              @change="fetchInvoices"
-            />
+              <el-input
+            suffix-icon="el-icon-search"
+            placeholder="Buscar..."
+            v-model="searchValue"
+            size="small"
+          />
             </el-form-item>
           </div>
         </div>
@@ -243,6 +238,220 @@
         </div>
         </div>
       </el-form>
+      <!--  -->
+     <el-dialog
+     title="Vista previa"
+     size="mini"
+     :visible.sync="showInvoicePreview"
+     width="900px"
+     :append-to-body="true"
+     >
+  <div class=" flex flex-col space-y-4">
+    <div class=" grid grid-cols-12 gap-4">
+            <div class="col-span-4 flex flex-col">
+              <span class="text-gray-700 font-bold text-sm w-full" >Tipo de documento</span>
+              <span>
+                {{ Object.keys(selectedInvoice).length > 0 ?  `${selectedInvoice.documentTypes.code} - ${selectedInvoice.documentType.name}` : ""
+                }}
+              </span>
+            </div>
+                 <div class=" col-span-2 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >No. autorización</span>
+       <span>{{ selectedInvoice ? selectedInvoice.authorization : ""}}</span>
+    </div>
+     <div class=" col-span-2 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" > Correlativo</span>
+       <span>{{ selectedInvoice   ? selectedInvoice.sequence : ""}}</span>
+    </div>
+     <div class=" col-span-2  flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" > Fecha de factura</span>
+       <span>{{ selectedInvoice   ? selectedInvoice.invoiceDate : ""}}</span>
+    </div>
+     <div class=" col-span-2   flex  flex-col">
+           <span class="font-semibold">Estado</span>
+            <div v-if="selectedInvoice">
+              <el-tag
+                size="small"
+                type="success"
+                v-if="selectedInvoice.isActiveInvoice"
+                >Activo</el-tag
+              >
+              <el-tag size="small" type="warning" v-else>Inactivo</el-tag>
+            </div>
+
+     </div>
+       
+    </div>
+  <div class=" grid grid-cols-12 gap-4">
+      <div class=" col-span-4 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Cliente:</span>
+       <span>{{ selectedInvoice ? selectedInvoice.customerName : ""}}</span>
+    </div>
+     <div class=" col-span-2 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Sucursal:</span>
+       <span> Casa matriz</span>
+    </div>
+     <div class=" col-span-3 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Condiciones de pago:</span>
+       <span>{{ selectedInvoice  ? `${selectedInvoice.paymentConditionName}` : ""}}</span>
+    </div>
+     <div class=" col-span-3 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Venta a cuenta de: </span>
+       <span>{{ selectedInvoice  ? `${selectedInvoice.sellerName}` : ""}}></span>
+    </div>
+  </div>
+
+       <div class=" grid grid-cols-12 gap-4">
+      <div class=" col-span-4 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Dirección 1:</span>
+       <span>{{ selectedInvoice ? selectedInvoice.customerAddress1 : ""}}</span>
+    </div>
+     <div class=" col-span-4 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Dirección 2:</span>
+       <span> {{selectedInvoice ? selectedInvoice.customerAddress2 :""}}</span>
+    </div>
+     <div class=" col-span-2 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Departamento:</span>
+       <span>{{ selectedInvoice  ? `${selectedInvoice.customerState}` : ""}}</span>
+    </div>
+     <div class=" col-span-2 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >Municipio: </span>
+       <span>{{ selectedInvoice  ? `${selectedInvoice.customerCity}` : ""}}</span>
+    </div>
+  </div>
+
+  
+       <div class=" grid grid-cols-12 gap-4">
+      <div class=" col-span-3 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >NIT:</span>
+       <span>{{ selectedInvoice ? selectedInvoice.customerNit : ""}}</span>
+    </div>
+     <div class=" col-span-3 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >DUI:</span>
+       <span> {{selectedInvoice ? selectedInvoice.customerDui : ""}}</span>
+    </div>
+     <div class=" col-span-2 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >NRC:</span>
+       <span>{{ selectedInvoice  ? `${selectedInvoice.customerNrc}` : ""}}</span>
+    </div>
+     <div class=" col-span-4 flex flex-col">
+       <span class="text-gray-700 font-bold text-sm w-full" >GIRO: </span>
+       <span>{{selectedInvoice  ? selectedInvoice.customerGiro : "" }}</span>
+    </div>
+  </div>
+   
+<el-table
+      :data="selectedInvoice.details"
+      style="width: 100%">
+       <el-table-column
+       type="index"
+        prop="quantity"
+        label="#"
+        width="30" 
+        />
+      <el-table-column
+        prop="quantity"
+        label="Cant."
+        width="55">
+      </el-table-column>
+      <el-table-column
+        prop="chargeDescription" 
+        label="Description"
+        width="375"/>
+     
+      <el-table-column
+        prop="unitPrice"
+        label="Precio Unit."
+        width="100"
+        >
+          <template slot-scope="scope">
+                  <span>{{
+                    calcUniPrice(selectedInvoice.documentType, scope.row)
+                      | formatMoney
+                  }}</span>
+                </template>
+      </el-table-column>
+       <el-table-column
+        prop="address"
+        label="V. No sujeta"
+        width="100"
+        >
+      </el-table-column>
+       <el-table-column
+        prop="address"
+        label="V. Extenta"
+        width="100"
+        >
+      </el-table-column>
+       <el-table-column
+        prop="ventaPrice"
+        label="V. Grabada" 
+        width="100"
+        >
+          <template slot-scope="scope">
+                  <span>{{
+                    calcUniPrice(selectedInvoice.ventaTotal, scope.row)
+                      | formatMoney
+                  }}</span>
+                </template>
+      </el-table-column>
+    </el-table>
+  
+   <!-- sumas -->
+        <table class="flex justify-end">
+          <tbody class="text-sm divide-y divide-gray-300">
+            <tr class="flex space-x-16">
+              <td align="right" class="text-blue-900 w-50">SUMAS:</td>
+              <td align="right" class="text-gray-800">
+                {{ selectedInvoice.sum | formatMoney }}
+              </td>
+            </tr>
+            <tr class="flex space-x-16" >
+              <td align="right" class="text-blue-900 w-50">13% Iva:</td>
+              <td align="right" class="text-gray-800">
+                {{ selectedInvoice.iva | formatMoney }}
+              </td>
+            </tr>
+            <tr class="flex space-x-16">
+              <td align="right" class="text-blue-900 w-50">Subtotal:</td>
+              <td align="right" class="text-gray-800">
+                {{ selectedInvoice.subtotal | formatMoney }}
+              </td>
+            </tr>
+            <tr class="flex space-x-16">
+              <td align="right" class="text-blue-900 w-50">Iva retenido:</td>
+              <td align="right" class="text-gray-800">
+                {{ selectedInvoice.ivaRetenido | formatMoney }}
+              </td>
+            </tr>
+            <tr class="flex space-x-16">
+              <td align="right" class="text-blue-900 w-50">Ventas exentas:</td>
+              <td align="right" class="text-gray-800">
+                {{ selectedInvoice.ventasExentas | formatMoney }}
+              </td>
+            </tr>
+            <tr class="flex space-x-16">
+              <td align="right" class="text-blue-900 w-50">
+                Ventas no sujetas:
+              </td>
+              <td align="right" class="text-gray-800">
+                {{ selectedInvoice.ventasNoSujetas  | formatMoney }}
+              </td>
+            </tr>
+            <tr class="flex space-x-16">
+              <td align="right" class="text-blue-900 font-semibold w-50">
+                Venta total:
+              </td>
+              <td align="right" class="text-gray-800">
+                {{ selectedInvoice.ventaTotal | formatMoney }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+  </div>
+
+     </el-dialog>
+     <!--  -->
         <el-table :data="invoices.invoices" stripe size="small">
         <el-table-column prop="index" min-width="40" />
         <el-table-column label="# Factura"   min-width="120">
@@ -255,7 +464,7 @@
         <!-- <el-table-column label="Tipo fact." prop="tipof" min-width="75" /> -->
         <el-table-column label="Fecha" prop="invoiceDate" min-width="90" />
         <el-table-column label="Cliente" prop="customerName" min-width="350" />
-        <el-table-column label="Estado" prop="true" min-width="80">
+        <el-table-column label="Estado" min-width="80">
           <template slot-scope="scope">
             <el-tag size="small" type="success" v-if="scope.row.isActiveInvoice"
               >Activo</el-tag
@@ -263,7 +472,11 @@
             <el-tag size="small" type="warning" v-else>Inactivo</el-tag>
           </template>
         </el-table-column>
-         <el-table-column label="total" prop="ventaTotal" min-width="80" />
+        <el-table-column label="Total" min-width="80" align="right">
+            <template slot-scope="scope">
+              <span>{{ scope.row.ventaTotal | formatMoney }}</span>
+            </template>
+          </el-table-column>
         <el-table-column label min-width="60" align="center">
           <template slot-scope="scope">
             <el-dropdown trigger="click" szie="mini">
@@ -291,7 +504,9 @@
                   class="text-red-500 font-semibold"
                   @click.native="deleteInvoice(scope.row)"
                 >
-                  <i class="el-icon-delete"></i> Eliminar cliente
+                  <span class=" text-red-500">
+                    <i class="el-icon-delete"></i> Eliminar cliente
+                  </span>
                 </el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
@@ -317,6 +532,7 @@
 <script>
 import LayoutContent from "../../components/layout/Content";
 import Notification from "../../components/Notification";
+import { selectValidation } from '../../tools';
 export default {
   name: "InvoicesIndex",
   components: { LayoutContent, Notification },
@@ -395,6 +611,7 @@ export default {
    fetchOnServer: false,
   data() {
     return {
+     r:[],
       loading: false,
       errorMessage: "",
       searchValue:"",
@@ -409,8 +626,11 @@ export default {
       inactiveService:   [],
       invoicesTotal:[],
       options:[],
+     showInvoicePreview:false,
+     selectedInvoice:{},
       invoices: {
         invoices: [],
+        details:[],
         count: 0,
       },
       page: {
@@ -430,7 +650,7 @@ export default {
   },
   methods: {
     fetchInvoices() {
-      let params = this.page;
+      let params = this.page.size;
       if (this.status !== "") {
         params = { ...params, active: this.status };
       }
@@ -451,6 +671,7 @@ export default {
       this.page.limit = val;
       this.fetchInvoices();
     },
+
     changeActive({ id, isActiveInvoice }) {
       const action = isActiveInvoice ? "desactivar" : "activar";
       this.$confirm(
@@ -533,6 +754,164 @@ export default {
       this.selectedInvoice = data.invoice;
       this.showInvoicePreview = true;
     },
+       calcUniPrice(documentType, { unitPrice, incTax, sellingType }) {
+      let uniPrice = null;
+      const amount = parseFloat(unitPrice);
+      let message = null;
+      if ((sellingType.id == 1) | (sellingType.id == 2)) {
+        unitPrice = amount;
+      } else {
+        if (documentType) {
+          switch (documentType.id) {
+            case 1:
+              unitPrice = amount * (incTax ? 1 : 1.13);
+              
+              break;
+            case 2:
+              unitPrice = amount / (incTax ? 1.13 : 1);
+             
+              break;
+          }
+        } else {
+          message = "Debe seleccionar un tipo de docuemnto";
+        }
+      }
+      return unitPrice;
+    },
+      //  const {data} = await this.$axios.get(`/business/${id}`);
+      //  this.selecetedBusiness =data.busine;
+      //  this.showInvoicePreview = true;
+    
+
   },
+   computed: {
+    filteredInvoices() {
+      const searchValue = this.searchValue.trim().toLowerCase();
+      const {
+       dateRange,
+      customer,
+      invoiceType,
+      status,
+      seller,
+      zone,
+      service,
+      } = this.filter;
+
+      let filteredInvoices = this.rawInvoices;
+
+      // Filtra por  buscador general
+      if (!R.isEmpty(searchValue)) {
+        filteredInvoices = filteredInvoices.filter(selectedInvoice => {
+          return (
+            selectedInvoice.customerName
+              .trim()
+              .toLowerCase()
+              .includes(searchValue) ||
+            selectedInvoice.status.name
+              .trim()
+              .toLowerCase()
+              .includes(searchValue) ||
+            selectedInvoice.ventaTotal
+              .toFixed(2)
+              .trim()
+              .toLowerCase()
+              .includes(searchValue) ||
+            selectedInvoice.documentType.code.toLowerCase().includes(searchValue) ||
+            selectedInvoice.authorization.toLowerCase().includes(searchValue) ||
+            selectedInvoice.sequence.toLowerCase().includes(searchValue)
+          );
+        });
+      }
+
+      // Filtra por rango de fechas
+      if (!R.isNil(dateRange)) {
+        filteredInvoices = filteredInvoices.filter(selectedInvoice => {
+          let date = new Date(selectedInvoice.invoiceDate);
+          return moment(date)
+            .add(1, "days")
+            .isBetween(dateRange[0], moment(dateRange[1]).add(1, "days"));
+        });
+      }
+
+      // Filtra por cliente
+      if (!R.isNil(customer)) {
+        filteredInvoices = filteredInvoices.filter(
+          selectedInvoice => selectedInvoice.customer.id === customer
+        );
+      }
+
+      // Filtra por tipo de factura
+      if (!R.isNil(invoiceDocumentType)) {
+        filteredInvoices = filteredInvoices.filter(
+          selectValidation => selectedInvoice.documentType.id === invoiceDocumentType
+        );
+      }
+
+      // Filtra por estado
+      if (!R.isNil(status)) {
+        filteredInvoices = filteredInvoices.filter(
+          selectedInvoice => selectedInvoice.status.id === status
+        );
+      }
+
+      // Filtra por vendedor
+      if (!R.isNil(seller)) {
+        filteredInvoices = filteredInvoices.filter(
+          selectedInvoice => selectedInvoice.invoicesSeller.id === seller
+        );
+      }
+
+      // Filtra por zona
+      if (!R.isNil(zone)) {
+        filteredInvoices = filteredInvoices.filter(
+          invoice => invoice.invoicesZone.id === zone
+        );
+      }
+
+      // Filtra por servicio
+      if (!R.isNil(service)) {
+        filteredInvoices = filteredInvoices.filter(
+          invoice => invoice.service.id === service
+        );
+      }
+
+      return filteredInvoices.sort((a, b) => {
+        const dateA = new Date(a.invoiceDate);
+        const dateB = new Date(b.invoiceDate);
+        return dateB - dateA;
+      });
+    },
+    paginatedInvoices() {
+      return this.filteredInvoices.slice(
+        this.page.number * this.page.size - this.page.size,
+        this.page.size * this.page.number
+      );
+    },
+    activeCustomers() {
+      return this.rawCustomers.filter(customer => customer.isActiveCustomer);
+    },
+    inactiveCustomers() {
+      return this.rawCustomers.filter(customer => !customer.isActiveCustomer);
+    },
+    activeServices() {
+      return this.rawServices.filter(service => service.active);
+    },
+    inactiveServices() {
+      return this.rawServices.filter(service => !service.active);
+    },
+    activeSellers() {
+      return this.rawSellers.filter(seller => seller.active);
+    },
+    inactiveSellers() {
+      return this.rawSellers.filter(seller => !seller.active);
+    },
+    activeZones() {
+      return this.rawZones.filter(zone => zone.active);
+    },
+    inactiveZones() {
+      return this.rawZones.filter(zone => !zone.active);
+    }
+  }
+
 };
 </script>
