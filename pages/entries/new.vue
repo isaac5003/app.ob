069cc5dac3f8ccc7ec4cfc954bfd14c08c77ecff
Isaac <isaac.cortez1294@hotmail.com>
2021-01-11 11:59:03 -0600

    OC-158: -Consumiento data de entryTypes en el-option

--- pages/entries/new.vue
+++ pages/entries/new.vue
@@ -7,23 +7,120 @@
       { name: 'Nueva partida', to: null },
     ]"
   >
+    <!-- dialogo editdetalledepartida-->
+    <el-dialog
+      title="Editar detalle de partida"
+      :visible.sync="showAccountingDetail"
+      width="550px"
+      :close-on-click-modal="false"
+      :append-to-body="true"
+    >
+      <el-form
+        :model="formAccountingDetail"
+        status-icon
+        :rules="entryDetailFormRules"
+        ref="formAccountingDetail"
+      >
+        <!-- first row -->
+        <div class="grid grid-cols-12">
+          <!-- cuenta contable -->
+          <div class="col-span-12">
+            <el-form-item label="Cuenta contable" prop="accountingCatalog">
+              <el-select
+                v-model="formAccountingDetail.accountingCatalog"
+                clearable
+                filterable
+                default-first-option
+                size="small"
+                class="w-full"
+                placeholder="Seleccionar"
+              >
+                <el-option
+                  v-for="e in entryTypes"
+                  :key="e.id"
+                  :label="`${e.code} - ${e.name}`"
+                  :value="e.id"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </div>
+        </div>
+        <!-- second row -->
+        <div class="grid grid-cols-12 gap-4">
+          <!-- concepto -->
+          <div class="col-span-12">
+            <el-form-item label="Concepto" prop="concept">
+              <el-input
+                v-model="formAccountingDetail.concept"
+                size="small"
+                autocomplete="off"
+                style="width: 100%"
+                :disabled="formAccountingDetail.accountingCatalog === ''"
+              />
+            </el-form-item>
+          </div>
+        </div>
+        <!-- third row -->
+        <div class="grid grid-cols-12 gap-4">
+          <!-- Cargo -->
+          <div class="col-span-6">
+            <el-form-item label="Cargo" prop="cargo">
+              <el-input-number
+                type="number"
+                :min="0"
+                :step="0.01"
+                v-model="formAccountingDetail.cargo"
+                size="small"
+                autocomplete="off"
+                style="width: 100%"
+                :disabled="formAccountingDetail.entryAccounting === ''"
+              />
+            </el-form-item>
+          </div>
+          <!-- Cargo -->
+          <div class="col-span-6">
+            <el-form-item label="Abono" prop="abono">
+              <el-input-number
+                type="number"
+                :min="0"
+                :step="0.01"
+                v-model="formAccountingDetail.abono"
+                size="small"
+                autocomplete="off"
+                style="width: 100%"
+                :disabled="formAccountingDetail.entryAccounting === ''"
+              />
+            </el-form-item>
+          </div>
+        </div>
+        <!-- boton guardar cancelar -->
+        <div class="flex justify-end dialog-footer">
+          <el-button type="primary" size="small">Guardar</el-button>
+          <el-button @click="showEditEntryDetail = false" size="small"
+            >Cancelar</el-button
+          >
+        </div>
+      </el-form>
+    </el-dialog>
+
     <div class="flex flex-col space-y-2">
       <el-form label-position="top">
         <!-- primer div tipo partida, correlativo y rango de fechas -->
         <div class="grid grid-cols-12 gap-4 relative">
-          <div class="col-start-1 col-span-4">
+          <div class="col-span-4">
             <el-form-item label="Tipo de patidad">
               <el-select
-                v-model="value"
+                v-model="formAccountingDetail.select"
                 size="small"
                 class="w-full"
                 placeholder="Select"
               >
                 <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
+                  v-for="i in entryTypes"
+                  :key="i.id"
+                  :label="`${i.code} - ${i.name}`"
+                  :value="i.id"
                 >
                 </el-option>
               </el-select>
@@ -43,11 +140,15 @@
           <div class="col-start-11 col-span-2">
             <el-form-item label="Rango de fechas:">
               <el-date-picker
-                v-model="option.name"
+                v-model="fecha"
                 size="small"
                 style="width: 100%"
                 type="date"
+                placeholder="Seleccionar"
+                :picker-options="pickerOptions"
+                format="dd MMMM yyyy"
               >
+                >
               </el-date-picker>
             </el-form-item>
           </div>
@@ -67,7 +168,7 @@
                 class="w-full"
                 border="1"
                 disabled="true"
-                >Option</el-checkbox
+                >Cuadrada</el-checkbox
               >
             </el-form-item>
           </div>
@@ -79,7 +180,7 @@
                 class="w-full"
                 border="1"
                 disabled="true"
-                >Option</el-checkbox
+                >Contabilizada</el-checkbox
               >
             </el-form-item>
           </div>
@@ -88,6 +189,7 @@
         <div class="grid grid-cols-12 gap-4 relative">
           <div class="col-start-11 col-span-2">
             <el-button
+              @click="showAccountingDetail = true"
               type="primary"
               icon="el-icon-plus"
               class="w-full"
@@ -136,6 +238,8 @@ import {
   inputValidation,
   selectValidation,
   checkBeforeLeave,
+  hasModule,
+  getIcon,
   checkBeforeEnter,
 } from "../../tools";
 import Notification from "../../components/Notification";
@@ -145,16 +249,133 @@ const storagekey = "new-customer";
 export default {
   name: "CustomerNew",
   components: { LayoutContent, Notification },
-  fetch() {},
+  fetch() {
+    const entryTypes = () => {
+      return this.$axios.get("/entries/types");
+    };
+
+    Promise.all([entryTypes()])
+      .then((res) => {
+        const [entryTypes] = res;
+        this.entryTypes = entryTypes.data.entryTypes;
+        console.log(entryTypes.data.entryTypes);
+        this.loading = false;
+      })
+      .catch((err) => {
+        this.errorMessage = err.response.data.message;
+      });
+  },
   data() {
+    const editCargoValidateCompare = (rule, value, callback) => {
+      const abono =
+        this.formAccountingDetail.abono > 0
+          ? this.formAccountingDetail.abono.toFixed(2)
+          : "";
+      const val = value > 0 ? value.toFixed(2) : "";
+      if (!abono) {
+        if (!val) {
+          callback(new Error("Este campo es requerido."));
+        } else {
+          callback();
+        }
+      } else if (abono && val) {
+        return callback(
+          new Error("No puedes agregar cargo y abono al mismo tiempo")
+        );
+      } else {
+        callback();
+      }
+    };
+    const editAbonoValidateCompare = (rule, value, callback) => {
+      const cargo =
+        this.formAccountingDetail.cargo > 0
+          ? this.formAccountingDetail.cargo.toFixed(2)
+          : "";
+      const val = value > 0 ? value.toFixed(2) : "";
+      if (!cargo) {
+        if (!val) {
+          callback(new Error("Este campo es requerido."));
+        } else {
+          callback();
+        }
+      } else if (cargo && val) {
+        return callback(
+          new Error("No puedes agregar cargo y abono al mismo tiempo")
+        );
+      } else {
+        callback();
+      }
+    };
     return {
-      loading: false,
+      entryTypes: [],
+      fecha: "",
+      showAccountingDetail: false,
+      formAccountingDetail: {
+        accountingCatalog: "",
+        concept: "",
+        cargo: "",
+        abono: "",
+      },
+      entryDetailFormRules: {
+        accountingCatalog: selectValidation(true),
+        concept: inputValidation(true),
+        cargo: [
+          {
+            validator: editCargoValidateCompare,
+            trigger: ["blur", "change"],
+          },
+        ],
+        abono: [
+          {
+            validator: editAbonoValidateCompare,
+            trigger: ["blur", "change"],
+          },
+        ],
+      },
+      // loading: false,
       option: {
         name: "",
       },
+      pickerOptions: {
+        // disabledDate(time) {
+        //   return time.getTime() < Date.now();
+        // },
+        dateNow(time) {
+          console.log(time);
+          return time.Date.now();
+        },
+        shortcuts: [
+          {
+            text: "Ahora",
+            onClick(picker) {
+              picker.$emit("pick", new Date());
+            },
+          },
+          {
+            text: "Ayer",
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit("pick", date);
+            },
+          },
+          {
+            text: "Mañana",
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit("pick", date);
+            },
+          },
+        ],
+      },
     };
   },
-  methods: {},
+  methods: {
+    closeDialog(name) {
+      this.$refs[name].resetFields();
+    },
+  },
   computed: {},
 };
 </script>
