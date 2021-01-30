c3e7ffac8b8b9c6744173bfbc2fd94d979b7b800
isaac <isaac.cortez1294@hotmail.com>
2021-01-29 18:20:00 -0600

    OC-261: -Reportes Generando reportes en PDF y EXCEL

---
+++ pages/customers/reports.vue
@@ -0,0 +1,301 @@
+<template>
+  <layout-content
+    v-loading="loading"
+    page-title="Reportes"
+    :breadcrumb="[
+      { name: 'Clientes', to: '/customers' },
+      { name: 'Reportes', to: null },
+    ]"
+  >
+    <div class="flex justify-center" v-if="errorMessage">
+      <Notification
+        class="w-1/2"
+        type="danger"
+        title="Error de comunicación"
+        :message="errorMessage"
+        :action="{
+          title: 'Intentar nuevamente',
+          function: () => $router.go(),
+        }"
+      />
+    </div>
+
+    <el-form
+      :model="reportForm"
+      :rules="reportFormRules"
+      ref="reportForm"
+      @submit.native.prevent="
+        generateReport('reportForm', reportForm)
+      "
+      status-icon
+    >
+      <!-- first row -->
+      <div class="grid grid-cols-12 gap-4">
+        <!-- Tipos de reporte -->
+        <div class="col-span-4">
+          <el-form-item label="Seleccione el reporte" prop="reportType">
+            <el-select
+              v-model="reportForm.reportType"
+              placeholder="Seleccione reporte"
+              size="small"
+              class="w-full"
+              default-first-option
+              filterable
+              clearable
+              @change="showRequirements(reportForm.reportType)"
+            >
+              <el-option
+                v-for="report in reports"
+                :key="report.id"
+                :label="report.name"
+                :value="report.id"
+              />
+            </el-select>
+          </el-form-item>
+        </div>
+
+        <div class="col-span-5">
+          <el-form-item prop="" label="Formato de reporte">
+            <el-radio-group
+              v-model="reportForm.radio"
+              :disabled="reportForm.reportType ? false : true"
+              class="w-full"
+            >
+              <el-radio border size="small" label="pdf">PDF</el-radio>
+              <el-radio border size="small" label="excel">Excel</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </div>
+      </div>
+
+
+      <!-- Guardar y Cancelar -->
+      <div class="grid grid-cols-12 gap-4">
+        <div class="col-start-9 col-span-2">
+          <el-button
+            :disabled="reportForm.reportType ? false : true"
+            type="primary"
+            class="w-full"
+            size="small"
+            icon="el-icon-download"
+            native-type="submit"
+            :loading="generating"
+            >Descargar</el-button
+          >
+        </div>
+        <div class="col-start-11 col-span-2">
+          <el-button class="w-full" size="small" @click="cancel()"
+            >Cancelar</el-button
+          >
+        </div>
+      </div>
+    </el-form>
+  </layout-content>
+</template>
+<script>
+import LayoutContent from "../../components/layout/Content";
+import Notification from "../../components/Notification";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import XLSX from "xlsx";
+import { selectValidation, getHeader, getFooter } from "../../tools";
+export default {
+  name: "EntriesReports",
+  components: {
+    LayoutContent,
+    Notification,
+  },
+  // fetch() {
+  //   const catalog = () => {
+  //     return this.$axios.get("/entries/catalog" );
+  //   };
+
+  //   Promise.all([catalog()])
+  //     .then((res) => {
+  //       const [catalog] = res;
+  //       this.catalog = catalog.data.catalog;
+  //       this.loading = false;
+  //     })
+  //     .catch((err) => {
+  //       this.errorMessage = err.response.data.message;
+  //     });
+  // },
+  // fetchOnServer: false,
+
+  data() {
+    return {
+      generating: false,
+      loading: false,
+      errorMessage: "",
+      reportForm: {
+        reportType: "",
+        radio: "pdf",
+      },
+      reportFormRules: {
+        reportType: selectValidation("change", true),
+        dateRange: selectValidation("change", true),
+      },
+      reports: [
+        { name: "Clientes", id: "clientes" },
+      ],
+      requirementForm: null,
+      accountingCatalog: [],
+    };
+  },
+
+  methods: {
+    showRequirements(id) {
+      if (!id) {
+        this.requirementForm = null;
+      } else {
+        switch (id) {
+          case "clientes":
+        }
+      }
+    },
+    generateReport(formName, { reportType, radio }) {
+      this.$refs[formName].validate((valid) => {
+        if (!valid) {
+          return false;
+        }
+
+        this.generating = true;
+        switch (reportType) {
+          case "clientes":
+            this.reportCustomers(radio);
+            break;
+        }
+      });
+    },
+    reportCustomers(fileType) {
+      const customers = () => this.$axios.get("/customers");
+      const bussinesInfo = () => this.$axios.get("/business/info");
+      switch (fileType) {
+        case "pdf":
+          Promise.all([customers(), bussinesInfo()]).then((res) => {
+            const [ customers, bussinesInfo] = res;
+            const customersData = customers.data.customers;
+            const { name, nit, nrc } = bussinesInfo.data.info;
+            const values = [];
+            const emptyRow = [{}, {}, {}, {}, {}];
+            console.log(customersData);
+
+            for(const c of customersData){
+              values.push(emptyRow);
+              values.push([
+                {
+                  text: c.name
+                },
+                {
+                  text: c.customerType.name
+                },
+                {
+                  text: c.nit
+                },
+                {
+                  text: c.nrc
+                },
+                {
+                  text: c.isActiveCustomer ? "Activo" : "Inactivo"
+                }
+              ]);
+            };
+            const docDefinition = {
+              pageSize: "LETTER",
+              pageOrientation: "portrait",
+              pageMargins: [20, 60, 20, 40],
+              header: getHeader(name, nit, nrc, null, "REPORTE CLIENTES"),
+              footer: getFooter(),
+              content: [
+                {
+                  fontSize: 9,
+                  layout: "noBorders",
+                  table: {
+                    headerRows: 1,
+                    widths: ["auto", "15%", "15%", "10%", "10%"],
+                    heights: -5,
+                    body: [
+                      [
+                        {
+                          text: "NOMBRE",
+                          style: "tableHeader",
+                        },
+                        {
+                          text: "TIPO",
+                          style: "tableHeader",
+                        },
+                        {
+                          text: "NIT",
+                          style: "tableHeader",
+                        },
+                        {
+                          text: "NRC",
+                          style: "tableHeader",
+                        },
+                        {
+                          text: "ESTADO",
+                          style: "tableHeader",
+                        },
+                      ],
+                      ...values,
+                    ],
+                  },
+                },
+              ],
+              styles: {
+                tableHeader: {
+                  bold: true,
+                  fontSize: 9,
+                },
+              },
+            };
+            this.generating = false;
+            pdfMake.createPdf(docDefinition).open();
+          });
+          break;
+        case "excel":
+          Promise.all([customers(), bussinesInfo()]).then((res) => {
+            const [ customers, bussinesInfo] = res;
+            const { name, nit, nrc } = bussinesInfo.data.info;
+            const customersData = customers.data.customers;
+            const values = [];
+            for (const c of customersData) {
+              values.push([
+                  c.name,
+                  c.customerType.name,
+                  c.nit,
+                  c.nrc,
+                  c.isActiveCustomer ? "Activo" : "Inactivo"
+              ]);
+            }
+            const document = [
+              [name],
+              ["REPORTE CLIENTES", `NIT: ${nit}`, `NRC: ${nrc}`],
+              [""],
+              ["NOMBRE", "TIPO", "NIT", "NRC", "ESTADO"],
+              ...values,
+            ];
+
+            const sheet = XLSX.utils.aoa_to_sheet(document);
+            const workbook = XLSX.utils.book_new();
+            const fileName = "report";
+            XLSX.utils.book_append_sheet(workbook, sheet, fileName);
+            XLSX.writeFile(workbook, `${fileName}.xlsx`);
+            this.generating = false;
+          });
+          break;
+      }
+    },
+    cancel() {
+      this.$confirm("¿Estás seguro que deseas salir?", "Confirmación", {
+        confirmButtonText: "Si, salir",
+        cancelButtonText: "Cancelar",
+        type: "warning",
+      }).then(() => {
+        this.$router.push("/entries");
+      });
+    },
+  },
+};
+</script>
