bc61bfe183609cab9d83da21fd6a81d5d0ce973d
Gerson E. Aguirre <gerson@ap-techno.com>
2021-02-01 00:44:31 -0600

    IMROVEMENTS - small fixes in tab ids for settings.

--- pages/entries/settings.vue
+++ pages/entries/settings.vue
@@ -712,8 +712,8 @@
           .catch(() => {})
       "
     >
-     <!--  tab generales -->
-    <el-tab-pane label="Generales" name="tagGeneral" >
+      <!--  tab generales -->
+      <el-tab-pane label="Generales" name="general">
         <!-- <div class="grid grid-cols-12">
           <div class="col-span-12">
             <Notification
@@ -724,60 +724,52 @@
             />
           </div>
         </div> -->
-    <el-form 
-    :model="fiscalPeriodForm" 
-    :rules="fiscalPeriodFormRules">
-      <div 
-      class="grid grid-cols-12 gap-4"
-      >
-          <el-form-item 
-          label="Ingrese el periodo fiscal de la empresa" 
-          prop="startDate"
-          class="col-span-4"
-          >
-          <el-date-picker
-           v-model="fiscalPeriodForm.startDate"
-           size="small"
-           type="month"
-           format="MMM-yyyy"
-           placeholder="Fecha inicial"
-           style="width:100%;">
-      </el-date-picker>
-          </el-form-item>
+        <el-form :model="fiscalPeriodForm" :rules="fiscalPeriodFormRules">
+          <div class="grid grid-cols-12 gap-4">
+            <el-form-item
+              label="Ingrese el periodo fiscal de la empresa"
+              prop="startDate"
+              class="col-span-4"
+            >
+              <el-date-picker
+                v-model="fiscalPeriodForm.startDate"
+                size="small"
+                type="month"
+                format="MMM-yyyy"
+                placeholder="Fecha inicial"
+                style="width: 100%"
+              >
+              </el-date-picker>
+            </el-form-item>
 
-          <el-form-item 
-          label=" " 
-          prop="endDate" 
-          class="col-span-4 mt-4"
-          >
-          <el-date-picker
-           v-model="fiscalPeriodForm.endDate"
-           size="small"
-           type="month"
-           format="MMM-yyyy"
-           placeholder="Fecha final"
-           style="width:100%;">
-      </el-date-picker>
-          </el-form-item>
-      </div>
+            <el-form-item label=" " prop="endDate" class="col-span-4 mt-4">
+              <el-date-picker
+                v-model="fiscalPeriodForm.endDate"
+                size="small"
+                type="month"
+                format="MMM-yyyy"
+                placeholder="Fecha final"
+                style="width: 100%"
+              >
+              </el-date-picker>
+            </el-form-item>
+          </div>
 
-        <div class="flex justify-end mt-4">
-          <el-button
-            type="primary"
-            size="small"
-            @click.native="submitResults(tablesData)"
-            >Guardar</el-button
-          >
-          <el-button 
-          size="small" 
-          @click="$router.push('/entries')">
-          Cancelar
-          </el-button>
-        </div>
-      </el-form> 
+          <div class="flex justify-end mt-4">
+            <el-button
+              type="primary"
+              size="small"
+              @click.native="submitResults(tablesData)"
+              >Guardar</el-button
+            >
+            <el-button size="small" @click="$router.push('/entries')">
+              Cancelar
+            </el-button>
+          </div>
+        </el-form>
       </el-tab-pane>
       <!-- Firmantes -->
-      <el-tab-pane label="Firmantes" >
+      <el-tab-pane label="Firmantes" name="signatures">
         <!-- <div class="grid grid-cols-12">
           <div class="col-span-12">
             <Notification
@@ -788,77 +780,75 @@
             />
           </div>
         </div> -->
-    <el-form
-    :model="firmantesForm"
-    :rules="firmantesFormRules"
-    >
-   <div class="grid grid-cols-12 gap-4">
-          <el-form-item 
-          label="Representante legal/Administrador único" 
-          prop="representante"
-          class="col-span-4"
-          >
-          <el-input
-           v-model="firmantesForm.representante"
-            type="text"
-           autocomplete="off"
-            maxlength="100"
-            minlength="5"
-            size="small"
-            show-word-limit
-            clearable
-            placeholder="">
-          </el-input>
-          </el-form-item>
-          <el-form-item 
-          label="Contador General" 
-          prop="contadorGeneral"
-          class="col-span-4"
-          >
-          <el-input
-           v-model="firmantesForm.contadorGeneral"
-           size="small"
-           class="w-full"
-           autocomplete="off"
-            maxlength="100"
-            minlength="5"
-            show-word-limit
-            filterable
-            clearable
-           placeholder="">
-          </el-input>
-          </el-form-item>
-        
-          <el-form-item 
-          label="Auditor"
-           prop="auditor"
-           class="col-span-4"
-           >
-          <el-input
-           v-model="firmantesForm.auditor"
-           size="small"
-           class="w-full"
-           autocomplete="off"
-            maxlength="100"
-            minlength="5"
-            show-word-limit
-            filterable
-            clearable
-           placeholder="">
-          </el-input>
-          </el-form-item>
-        </div>
-        <div class="flex justify-end mt-4">
-          <el-button
-            type="primary"
-            size="small"
-            @click.native="submitResults(tablesData)"
-            >Guardar</el-button
-          >
-          <el-button size="small" @click="$router.push('/entries')">Cancelar</el-button>
-        </div>
-      </el-form>
-</el-tab-pane>
+        <el-form :model="firmantesForm" :rules="firmantesFormRules">
+          <div class="grid grid-cols-12 gap-4">
+            <el-form-item
+              label="Representante legal/Administrador único"
+              prop="representante"
+              class="col-span-4"
+            >
+              <el-input
+                v-model="firmantesForm.representante"
+                type="text"
+                autocomplete="off"
+                maxlength="100"
+                minlength="5"
+                size="small"
+                show-word-limit
+                clearable
+                placeholder=""
+              >
+              </el-input>
+            </el-form-item>
+            <el-form-item
+              label="Contador General"
+              prop="contadorGeneral"
+              class="col-span-4"
+            >
+              <el-input
+                v-model="firmantesForm.contadorGeneral"
+                size="small"
+                class="w-full"
+                autocomplete="off"
+                maxlength="100"
+                minlength="5"
+                show-word-limit
+                filterable
+                clearable
+                placeholder=""
+              >
+              </el-input>
+            </el-form-item>
+
+            <el-form-item label="Auditor" prop="auditor" class="col-span-4">
+              <el-input
+                v-model="firmantesForm.auditor"
+                size="small"
+                class="w-full"
+                autocomplete="off"
+                maxlength="100"
+                minlength="5"
+                show-word-limit
+                filterable
+                clearable
+                placeholder=""
+              >
+              </el-input>
+            </el-form-item>
+          </div>
+          <div class="flex justify-end mt-4">
+            <el-button
+              type="primary"
+              size="small"
+              @click.native="submitResults(tablesData)"
+              >Guardar</el-button
+            >
+            <el-button size="small" @click="$router.push('/entries')"
+              >Cancelar</el-button
+            >
+          </div>
+        </el-form>
+      </el-tab-pane>
       <!-- tab catalogo -->
       <el-tab-pane label="Catálogo de cuentas" name="catalog" class="space-y-4">
         <!-- first row -->
@@ -1149,7 +1139,7 @@
       </el-tab-pane>
 
       <!-- tab estado resultados -->
-      <el-tab-pane label="Estado de resultados" name="estado resultados">
+      <el-tab-pane label="Estado de resultados" name="estado-resultados">
         <div class="grid grid-cols-12">
           <div class="col-span-12">
             <Notification
@@ -1328,7 +1318,7 @@
 </template>
 
 <script>
-import { format } from 'date-fns';
+import { format } from "date-fns";
 import LayoutContent from "../../components/layout/Content";
 import Notification from "../../components/Notification";
 import { getIcon, hasModule } from "../../tools";
@@ -1406,10 +1396,9 @@ export default {
       }
     };
     const startDateValidateCompare = (rule, value, callback) => {
-      const startDate =
-        this.fiscalPeriodForm.startDate
-          ? new Date(this.fiscalPeriodForm.startDate)
-          : "";
+      const startDate = this.fiscalPeriodForm.startDate
+        ? new Date(this.fiscalPeriodForm.startDate)
+        : "";
       const val = value ? new Date(value) : "";
       if (!startDate) {
         if (!val) {
@@ -1418,18 +1407,15 @@ export default {
           callback();
         }
       } else if (startDate > val) {
-        return callback(
-          new Error("La fecha inicial no puede ser mayor")
-        );
+        return callback(new Error("La fecha inicial no puede ser mayor"));
       } else {
         callback();
       }
     };
-     const endDateValidateCompare = (rule, value, callback) => {
-      const endDate =
-        this.fiscalPeriodForm.endDate
-          ? new Date(this.fiscalPeriodForm.endDate)
-          : "";
+    const endDateValidateCompare = (rule, value, callback) => {
+      const endDate = this.fiscalPeriodForm.endDate
+        ? new Date(this.fiscalPeriodForm.endDate)
+        : "";
       const val = value ? new Date(value) : "";
       if (!endDate) {
         if (!val) {
@@ -1438,51 +1424,45 @@ export default {
           callback();
         }
       } else if (endDate < val) {
-        return callback(
-          new Error("La fecha final no puede ser menor")
-        );
+        return callback(new Error("La fecha final no puede ser menor"));
       } else {
         callback();
       }
     };
-   
+
     return {
       pageloading: true,
       tableloading: false,
-      tab: "tagGeneral",
-      utab: "invoicing",  
-      date: new Date(2021,2),
-       firmantesForm:{
-        representante:"",
-        contadorGeneral:"",
-        auditor:"",
-        value1: '',
-        value2:{
-          name:{
-            
-          }
-        }
-       },
-       
-      firmantesFormRules:{
-        representante:inputValidation(true,5,100),
-        contadorGeneral:inputValidation(true,5,100),
-        auditor:inputValidation(true,5,100),
-        
-        
+      tab: "general",
+      utab: "invoicing",
+      date: new Date(2021, 2),
+      firmantesForm: {
+        representante: "",
+        contadorGeneral: "",
+        auditor: "",
+        value1: "",
+        value2: {
+          name: {},
+        },
       },
-      fiscalPeriodForm:{
+
+      firmantesFormRules: {
+        representante: inputValidation(true, 5, 100),
+        contadorGeneral: inputValidation(true, 5, 100),
+        auditor: inputValidation(true, 5, 100),
+      },
+      fiscalPeriodForm: {
         startDate: "",
-        endDate: ""
+        endDate: "",
       },
-      fiscalPeriodFormRules:{
+      fiscalPeriodFormRules: {
         startDate: [
           {
             validator: startDateValidateCompare,
             trigger: ["blur", "change"],
           },
         ],
-         endDate: [
+        endDate: [
           {
             validator: endDateValidateCompare,
             trigger: ["blur", "change"],
