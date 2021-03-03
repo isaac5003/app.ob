501f981a3f473e008bdbd07389e5746da88a4e3f
jorge vladimir betancourt soriano <jobeta1996@gmail.com>
2021-03-03 10:50:40 -0600

    OB-96 change of design

--- pages/customers/settings.vue
+++ pages/customers/settings.vue
@@ -16,73 +16,15 @@
           })
           .catch(() => {})
       "
-     >
-      <el-tab-pane label="Integraciones" name="integrations" class="space-y-3">
-        <div class="grid grid-cols-12">
-          <div class=" col-span-12">
-            <Notification
+    >
+      <el-tab-pane label="Integraciones" name="integrations" class="space-y-2">
+        <Notification
           class="w-full"
           type="info"
           title="Integraciones"
-          message="En esta sección se realizan las configuraciones de integración con otros modulos de manera general. Estas configuraciones se aplicarán a todos los clientes que no tengan una configuración individual."
-          
+          message="En esta sección se realizan las configuraciones de integración con otros modulos de manera general. Estas configuraciones se aplicarán a todos los servicios que no tengan una configuración individual."
         />
-        </div>
-          </div>
-      
-      <div class=" flex flex-col space-y-2">
-          <div class="grid grid-cols-12 gap-4  ">
-            <el-form
-             label-position="top"
-             >
-             <div class="w-56 ">
-               <template>
-                    <el-form-item
-               label="Seleccione una cuenta"
-               >
-               <el-select
-               v-model="cogInfo"
-               placeholder=" seleccione una cuenta"
-               clearable
-               filterable
-               class="w-full"
-               default-first-option
-               size="small"
-               >
-               <el-option
-               v-for="c in cogSettings"
-               :key="c.id"
-               :label="c.name"
-               :value="c.id"
-
-               >
-
-               </el-option>
-               </el-select>
-               </el-form-item>
-               </template>
-            
-             </div>
-
-             <div class=" flex-row justify-items-end mx-33">
-               <div class=" flex flex-row justify-items-end mx-160">
-               <el-button
-               type="primary"
-               size="small"
-               > Guardar</el-button>
-               <el-button
-               size="small"
-               
-               >Cancelar</el-button>
-             </div>
-             </div>
-             
-            </el-form>
-          </div>
-        </div>
-     
-
-        <!-- <el-tabs
+        <el-tabs
           tab-position="left"
           v-model="utab"
           @tab-click="
@@ -94,31 +36,45 @@
               .catch(() => {})
           "
         >
-          <el-tab-pane
-            v-for="(integration, k) of filteredIntegrations"
-            :key="k"
-            :name="integration.id"
-          >
-            <span slot="label" class="flex items-center justify-between"
-              ><svg
-                class="w-5 h-5 mr-2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.5"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                v-html="integration.icon"
-              />
-              {{ integration.name }}</span
-            >
-            {{ integration.name }}
-          </el-tab-pane>
-        </el-tabs> -->
+          <el-form label-position="top">
+            <div class="grid grid-cols-12 gap-4">
+              <el-form-item label="Seleccione una cuenta" class="col-span-4">
+                <template>
+                  <el-select
+                    v-model="cogInfo"
+                    placeholder="Seleccione una cuenta"
+                    size="small"
+                    clearable
+                    filterable
+                    class="w-full"
+                  >
+                    <el-option
+                      v-for="c in cogSetting"
+                      :key="c.id"
+                      :label="c.name"
+                      :value="c.id"
+                    >
+                    </el-option>
+                  </el-select>
+                </template>
+              </el-form-item>
+            </div>
+            <div class="flex justify-end">
+              <el-button
+                type="primary"
+                size="small"
+                native-type="submit"
+                :loading="generating"
+                >Guardar</el-button
+              >
+              <el-button size="small" @click="$router.push('/customers')"
+                >Cancelar</el-button
+              >
+            </div>
+          </el-form>
+        </el-tabs>
       </el-tab-pane>
-      
-    </el-tabs> 
-    
+    </el-tabs>
   </layout-content>
 </template>
 
@@ -131,42 +87,20 @@ export default {
   name: "CustomerSettings",
   components: { LayoutContent, Notification },
   fetch() {
-    if (this.$route.query.tab) {
-      this.tab = this.$route.query.tab;
-    }
-    if (this.$route.query.utab) {
-      this.utab = this.$route.query.utab;
-    }
+   
   },
   fetchOnServer: false,
   data() {
     return {
       cogInfo:"",
-      cogSettings:[
-        {name:'cogSettings'},
-      ],
+      cogSetting:[{name:"Isaac"}],
       tab: "integrations",
       utab: "invoicing",
-      integrations: [
-        {
-          name: "Facturación",
-          ref: "cfb8addb-541b-482f-8fa1-dfe5db03fdf4",
-          id: "invoicing",
-          icon: getIcon("dolar"),
-        },
-        {
-          name: "Contabilidad",
-          ref: "a98b98e6-b2d5-42a3-853d-9516f64eade8",
-          id: "accounting",
-          icon: getIcon("cash"),
-        },
-      ],
+    
     };
   },
   computed: {
-    filteredIntegrations() {
-      return this.integrations.filter((i) => hasModule(i.ref, this.$auth.user));
-    },
+  
   },
 };
 </script>
