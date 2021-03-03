0559164d301f5895b88273a55cf23b5fbcbbce95
jorge vladimir betancourt soriano <jobeta1996@gmail.com>
2021-03-03 09:58:41 -0600

    -OB-97-cambio de diseño.

--- pages/services/settings.vue
+++ pages/services/settings.vue
@@ -17,7 +17,7 @@
           .catch(() => {})
       "
     >
-      <el-tab-pane label="Integraciones" name="integrations" class="space-y-3">
+      <el-tab-pane label="Integraciones" name="integrations" class="space-y-2">
         <Notification
           class="w-full"
           type="info"
@@ -36,68 +36,42 @@
               .catch(() => {})
           "
         >
-          <div class="flex flex-col space-y-2">
+          <el-form label-position="top">
             <div class="grid grid-cols-12 gap-4">
-              <div class="col-span-3">
-                <el-form label-position="top">
-                  <el-form-item label="Seleccione una cuenta">
-                    <template>
-                      <el-select
-                        v-model="cogIfo"
-                        placeholder="Seleccione una cuenta"
-                        size="small"
-                        clearable
-                        filterable
-                        class="w-full"
-                      >
-                        <el-option
-                          v-for="c in cogSetting"
-                          :key="c.id"
-                          :label="c.name"
-                          :value="c.id"
-                        >
-                        </el-option>
-                      </el-select>
-                    </template>
-                  </el-form-item>
-                </el-form>
-              </div>
+              <el-form-item label="Seleccione una cuenta" class="col-span-4">
+                <template>
+                  <el-select
+                    v-model="cogIfo"
+                    placeholder="Seleccione una cuenta"
+                    size="small"
+                    clearable
+                    filterable
+                    class="w-full"
+                  >
+                    <el-option
+                      v-for="c in cogSetting"
+                      :key="c.id"
+                      :label="c.name"
+                      :value="c.id"
+                    >
+                    </el-option>
+                  </el-select>
+                </template>
+              </el-form-item>
             </div>
-           
-      <div class="flex flex-row justify-end">
-        <el-button
-          type="primary"
-          size="small"
-          native-type="submit"
-          :loading="generating"
-          >Guardar</el-button
-        >
-        <el-button size="small" @click="$router.push('/services')"
-          >Cancelar</el-button
-        >
-        </div>
-          </div>
-
-          <!-- <el-tab-pane
-            v-for="(integration, k) of filteredIntegrations"
-            :key="k"
-            :name="integration.id"
-          >
-            <span slot="label" class="flex items-center justify-between"
-              ><svg
-                class="w-5 h-5 mr-2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.5"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                v-html="integration.icon"
-              />
-              {{ integration.name }}</span
-            >
-            {{ integration.name }}
-          </el-tab-pane> -->
+            <div class="flex flex-row justify-end">
+              <el-button
+                type="primary"
+                size="small"
+                native-type="submit"
+                :loading="generating"
+                >Guardar</el-button
+              >
+              <el-button size="small" @click="$router.push('/services')"
+                >Cancelar</el-button
+              >
+            </div>
+          </el-form>
         </el-tabs>
       </el-tab-pane>
     </el-tabs>
@@ -112,14 +86,7 @@ import { getIcon, hasModule } from "../../tools";
 export default {
   name: "CustomerSettings",
   components: { LayoutContent, Notification },
-  fetch() {
-    if (this.$route.query.tab) {
-      this.tab = this.$route.query.tab;
-    }
-    if (this.$route.query.utab) {
-      this.utab = this.$route.query.utab;
-    }
-  },
+  fetch() {},
   fetchOnServer: false,
   data() {
     return {
@@ -144,20 +111,8 @@ export default {
       ],
       tab: "integrations",
       utab: "invoicing",
-      integrations: [
-        {
-          name: "Contabilidad",
-          ref: "a98b98e6-b2d5-42a3-853d-9516f64eade8",
-          id: "accounting",
-          icon: getIcon("cash"),
-        },
-      ],
     };
   },
-  computed: {
-    filteredIntegrations() {
-      return this.integrations.filter((i) => hasModule(i.ref, this.$auth.user));
-    },
-  },
+  computed: {},
 };
 </script>
