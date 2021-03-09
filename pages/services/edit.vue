3a07dd2fb8d40fb83076cffec928408a950a24c5
Gerson E. Aguirre <gerson@ap-techno.com>
2021-03-09 10:30:31 -0600

    COMPLETED - Now it is what it should be.

--- pages/services/edit.vue
+++ pages/services/edit.vue
@@ -7,19 +7,6 @@
       { name: 'Editar servicio', to: null },
     ]"
   >
-
-  <el-tabs
-   @click="
-   $router
-          .replace({
-            path: `/services/edit`,
-            query: { tab },
-          })
-          .catch(() => {})
-      "
-  >
-  <el-tab-pane label="Información general">
-    <div class=" flex flex-col space-y-2 ">
     <el-form
       :model="servicesEditForm"
       :rules="servicesEditFormRules"
@@ -29,123 +16,134 @@
         submitEditService('servicesEditForm', servicesEditForm)
       "
     >
-      <div class="grid grid-cols-12 gap-4">
-        <el-form-item
-          label="Nombre del servicio"
-          prop="name"
-          class="col-span-5"
-        >
-          <el-input
-            ref="name"
-            type="text"
-            v-model="servicesEditForm.name"
-            size="small"
-            autocomplete="off"
-            minlength="5"
-            maxlength="60"
-            show-word-limit
-          />
-        </el-form-item>
-        <el-form-item label="Costo" prop="cost" class="col-span-2">
-          <el-input-number
-            ref="cost"
-            type="number"
-            :min="0.01"
-            :step="0.01"
-            v-model="servicesEditForm.cost"
-            size="small"
-            autocomplete="off"
-            style="width: 100%"
-          />
-        </el-form-item>
-        <el-form-item
-          label="Tipo de venta"
-          prop="sellingType"
-          class="col-span-5"
-        >
-          <el-radio-group
-            ref="sellingType"
-            v-model="servicesEditForm.sellingType"
-            class="w-full"
-          >
-            <el-row :gutter="15">
-              <el-col :span="8" v-for="(s, k) in sellingTypes" :key="k">
-                <el-radio :label="s.id" border class="w-full" size="small">{{
-                  s.name
-                }}</el-radio>
-              </el-col>
-            </el-row>
-          </el-radio-group>
-        </el-form-item>
-      </div>
-      <el-form-item label="Descripción del servicio" prop="description">
-        <el-input
-          ref="description"
-          type="textarea"
-          :rows="4"
-          v-model="servicesEditForm.description"
-          minlength="5"
-          maxlength="5000"
-          show-word-limit
-          class="mt-1"
-        />
-      </el-form-item>
-      <div class="flex justify-end">
-        <el-button type="primary" size="small" native-type="submit"
-          >Guardar</el-button
-        >
-        <el-button size="small" @click="$router.push('/services')"
-          >Cancelar</el-button
-        >
-      </div>
-    </el-form>
-  </div>
-  </el-tab-pane>
-
-  <el-tab-pane  label="Integraciones">
-         <el-form label-position="top">
-            <div class="grid grid-cols-12 gap-4">
-              <el-form-item label="Seleccione una cuenta" class="col-span-4">
-                <template>
-                  <el-select
-                    v-model="cogInfo"
-                    placeholder="Seleccione una cuenta"
-                    size="small"
-                    clearable
-                    filterable
-                    class="w-full"
-                  >
-                    <el-option
-                      v-for="c in cogSetting"
-                      :key="c.id"
-                      :label="c.name"
-                      :value="c.id"
-                    >
-                    </el-option>
-                  </el-select>
-                </template>
-              </el-form-item>
-            </div>
-            <div class="flex justify-end">
-              <el-button
-                type="primary"
+      <el-tabs
+        v-model="tab"
+        @tab-click="
+          $router
+            .replace({
+              path: `/services/edit`,
+              query: { ref: $route.query.ref, tab },
+            })
+            .catch(() => {})
+        "
+      >
+        <el-tab-pane label="Información general" name="general-information">
+          <div class="grid grid-cols-12 gap-4">
+            <el-form-item
+              label="Nombre del servicio"
+              prop="name"
+              class="col-span-5"
+            >
+              <el-input
+                ref="name"
+                type="text"
+                v-model="servicesEditForm.name"
                 size="small"
-                native-type="submit"
-                :loading="generating"
-                >Guardar</el-button
+                autocomplete="off"
+                minlength="5"
+                maxlength="60"
+                show-word-limit
+              />
+            </el-form-item>
+            <el-form-item label="Costo" prop="cost" class="col-span-2">
+              <el-input-number
+                ref="cost"
+                type="number"
+                :min="0.01"
+                :step="0.01"
+                v-model="servicesEditForm.cost"
+                size="small"
+                autocomplete="off"
+                style="width: 100%"
+              />
+            </el-form-item>
+            <el-form-item
+              label="Tipo de venta"
+              prop="sellingType"
+              class="col-span-5"
+            >
+              <el-radio-group
+                ref="sellingType"
+                v-model="servicesEditForm.sellingType"
+                class="w-full"
               >
-              <el-button size="small" @click="$router.push('/services')"
-                >Cancelar</el-button
+                <el-row :gutter="15">
+                  <el-col :span="8" v-for="(s, k) in sellingTypes" :key="k">
+                    <el-radio
+                      :label="s.id"
+                      border
+                      class="w-full"
+                      size="small"
+                      >{{ s.name }}</el-radio
+                    >
+                  </el-col>
+                </el-row>
+              </el-radio-group>
+            </el-form-item>
+          </div>
+          <el-form-item label="Descripción del servicio" prop="description">
+            <el-input
+              ref="description"
+              type="textarea"
+              :rows="4"
+              v-model="servicesEditForm.description"
+              minlength="5"
+              maxlength="5000"
+              show-word-limit
+              class="mt-1"
+            />
+          </el-form-item>
+        </el-tab-pane>
+
+        <el-tab-pane
+          label="Integraciones"
+          name="integrations"
+          class="space-y-4"
+        >
+          <Notification
+            class="w-full"
+            type="info"
+            title="Información"
+            message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          />
+          <div class="grid grid-cols-12 gap-4">
+            <el-form-item label="Seleccione una cuenta" class="col-span-4">
+              <el-select
+                v-model="cogInfo"
+                placeholder="Seleccione una cuenta"
+                size="small"
+                clearable
+                filterable
+                class="w-full"
               >
-            </div>
-          </el-form>
-      </el-tab-pane>
-  </el-tabs>
+                <el-option
+                  v-for="c in cogSetting"
+                  :key="c.id"
+                  :label="c.name"
+                  :value="c.id"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </div>
+        </el-tab-pane>
+
+        <div class="flex justify-end">
+          <el-button type="primary" size="small" native-type="submit"
+            >Guardar</el-button
+          >
+          <el-button size="small" @click="$router.push('/services')"
+            >Cancelar</el-button
+          >
+        </div>
+      </el-tabs>
+    </el-form>
   </layout-content>
 </template>
 
 <script>
 import LayoutContent from "../../components/layout/Content";
+import Notification from "../../components/Notification";
 import {
   checkBeforeEnter,
   checkBeforeLeave,
@@ -160,10 +158,11 @@ export default {
   head: {
     titleTemplate: `%s | Editar servicio`,
   },
-  components: { LayoutContent },
+  components: { LayoutContent, Notification },
   fetch() {
-
-
+    if (this.$route.query.tab) {
+      this.tab = this.$route.query.tab;
+    }
     const sellingTypes = () => this.$axios.get("/services/selling-types");
     const service = () => this.$axios.get(`/services/${this.$route.query.ref}`);
 
@@ -189,14 +188,16 @@ export default {
   },
   data() {
     return {
-       cogInfo:"",
-       cogSetting:[{name:"Isaac"}],
-      tab: "infoGeneral",
-       acountGeneral:"",
-       acountInfo:[{
-         id:1,
-         name:'Jorge_vladimir@hotmail.com'
-       }],
+      cogInfo: "",
+      cogSetting: [{ name: "Isaac" }],
+      tab: "general-information",
+      acountGeneral: "",
+      acountInfo: [
+        {
+          id: 1,
+          name: "Jorge_vladimir@hotmail.com",
+        },
+      ],
       pageloading: true,
       sellingTypes: [],
       servicesEditForm: {
