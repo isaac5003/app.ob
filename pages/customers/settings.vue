4d49cb9ba20eedd1084c68cd93813eb2fd173ec0
Gerson E. Aguirre <gerson@ap-techno.com>
2021-03-09 10:39:46 -0600

    COMPLETED - Some small changes.

--- pages/customers/settings.vue
+++ pages/customers/settings.vue
@@ -6,101 +6,81 @@
       { name: 'Configuraciones', to: null },
     ]"
   >
-    <el-tabs
-      v-model="tab"
-      @tab-click="
-        $router
-          .replace({
-            path: `/customers/settings`,
-            query: { tab },
-          })
-          .catch(() => {})
-      "
-    >
-      <el-tab-pane label="Integraciones" name="integrations" class="space-y-2">
-        <Notification
-          class="w-full"
-          type="info"
-          title="Integraciones"
-          message="En esta sección se realizan las configuraciones de integración con otros modulos de manera general. Estas configuraciones se aplicarán a todos los servicios que no tengan una configuración individual."
-        />
-        <el-tabs
-          tab-position="left"
-          v-model="utab"
-          @tab-click="
-            $router
-              .replace({
-                path: `/customers/settings`,
-                query: { tab, utab },
-              })
-              .catch(() => {})
-          "
+    <el-form label-position="top">
+      <el-tabs
+        v-model="tab"
+        @tab-click="
+          $router
+            .replace({
+              path: `/customers/settings`,
+              query: { tab },
+            })
+            .catch(() => {})
+        "
+      >
+        <el-tab-pane
+          label="Integraciones"
+          name="integrations"
+          class="space-y-2"
         >
-          <el-form label-position="top">
-            <div class="grid grid-cols-12 gap-4">
-              <el-form-item label="Seleccione una cuenta" class="col-span-4">
-                <template>
-                  <el-select
-                    v-model="cogInfo"
-                    placeholder="Seleccione una cuenta"
-                    size="small"
-                    clearable
-                    filterable
-                    class="w-full"
-                  >
-                    <el-option
-                      v-for="c in cogSetting"
-                      :key="c.id"
-                      :label="c.name"
-                      :value="c.id"
-                    >
-                    </el-option>
-                  </el-select>
-                </template>
-              </el-form-item>
-            </div>
-            <div class="flex justify-end">
-              <el-button
-                type="primary"
+          <Notification
+            class="w-full"
+            type="info"
+            title="Integraciones"
+            message="En esta sección se realizan las configuraciones de integración con otros modulos de manera general. Estas configuraciones se aplicarán a todos los servicios que no tengan una configuración individual."
+          />
+          <div class="grid grid-cols-12 gap-4">
+            <el-form-item label="Seleccione una cuenta" class="col-span-4">
+              <el-select
+                v-model="cogInfo"
+                placeholder="Seleccione una cuenta"
                 size="small"
-                native-type="submit"
-                :loading="generating"
-                >Guardar</el-button
+                clearable
+                filterable
+                class="w-full"
               >
-              <el-button size="small" @click="$router.push('/customers')"
-                >Cancelar</el-button
-              >
-            </div>
-          </el-form>
-        </el-tabs>
-      </el-tab-pane>
-    </el-tabs>
+                <el-option
+                  v-for="c in cogSetting"
+                  :key="c.id"
+                  :label="c.name"
+                  :value="c.id"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </div>
+        </el-tab-pane>
+
+        <div class="flex justify-end">
+          <el-button type="primary" size="small" native-type="submit"
+            >Guardar</el-button
+          >
+          <el-button size="small" @click="$router.push('/customers')"
+            >Cancelar</el-button
+          >
+        </div>
+      </el-tabs>
+    </el-form>
   </layout-content>
 </template>
 
 <script>
 import LayoutContent from "../../components/layout/Content";
 import Notification from "../../components/Notification";
-import { getIcon, hasModule } from "../../tools";
 
 export default {
   name: "CustomerSettings",
   components: { LayoutContent, Notification },
-  fetch() {
-   
-  },
+  fetch() {},
   fetchOnServer: false,
   data() {
     return {
-      cogInfo:"",
-      cogSetting:[{name:"Isaac"}],
+      cogInfo: "",
+      cogSetting: [{ name: "Isaac" }],
       tab: "integrations",
-      utab: "invoicing",
-    
     };
   },
-  computed: {
-  
-  },
+  computed: {},
 };
 </script>
+s
