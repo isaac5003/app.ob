7cdeb2c4cb1b8afebe6148df53d3ca241e6bb6d1
isaac <isaac.cortez1294@hotmail.com>
2021-03-27 15:43:05 -0600

    OB-204: -Agregando pantalla principal de proveedores -Menú de pantalla procipal

--- components/layout/Menu.vue
+++ components/layout/Menu.vue
@@ -11,9 +11,7 @@
         class="item"
         effect="dark"
         :open-delay="1000"
-        :content="
-          `${$auth.user.workspace.company.name} | ${$auth.user.workspace.branch.name}`
-        "
+        :content="`${$auth.user.workspace.company.name} | ${$auth.user.workspace.branch.name}`"
         placement="right"
       >
         <el-popover
@@ -152,10 +150,8 @@
       <el-tooltip
         class="item"
         effect="dark"
-        :content="
-          `${$auth.user.names} 
-        ${$auth.user.lastnames}`
-        "
+        :content="`${$auth.user.names} 
+        ${$auth.user.lastnames}`"
         placement="right"
       >
         <nuxt-link
@@ -217,6 +213,14 @@ export default {
           path: "/entries",
           name: "Contabilidad",
           icon: getIcon("cash"),
+          exact: false,
+        },
+        {
+          always: false,
+          id: "a98b98e6-b2d5-42a3-853d-9516f64eade8",
+          path: "/providers",
+          name: "Proveedores",
+          icon: getIcon("truck"),
           exact: false,
         },
       ],
