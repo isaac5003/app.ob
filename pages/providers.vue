13db22b9b4636582a4845463e59b3cf03ebffd62
jorge <jobeta1996@gmail.com>
2021-06-03 20:42:20 -0600

    OB-353: Agregando el metodo hasModule para casa integracion

--- pages/providers.vue
+++ pages/providers.vue
@@ -7,10 +7,17 @@
 
 <script>
 import LayoutSubmenu from "../components/layout/Submenu";
+import { hasModule } from "../../tools/index.js";
 import { getIcon } from "../tools";
 export default {
   name: "Providers",
   components: { LayoutSubmenu },
+
+  fetch() {
+    if (!hasModule(["f6000cbb-1e6d-4f7d-a7cc-cadd78d23076"], this.$auth.user)) {
+      this.menu = this.menu.slice(0, -1);
+    }
+  },
   data() {
     return {
       menu: [
